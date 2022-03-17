54984dbf4fd89125aa67de082ad06e06a11dfc40
Alisson Youssf <youssfbr@gmail.com>
2022-03-16 21:27:31 -0300

    Backend project created

---
+++ backend/src/main/java/com/github/youssfbr/votacaobbbapi/VotacaoBbbApiApplication.java
@@ -0,0 +1,13 @@
+package com.github.youssfbr.votacaobbbapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VotacaoBbbApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VotacaoBbbApiApplication.class, args);
+	}
+
+}
