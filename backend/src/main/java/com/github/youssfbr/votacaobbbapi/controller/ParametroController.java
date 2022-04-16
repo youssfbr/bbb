c47fbd7612f46d3b46972a1bef60faa0f277308e
Alisson Youssf <youssfbr@gmail.com>
2022-04-16 19:07:23 -0300

    Alteracoes

--- backend/src/main/java/com/github/youssfbr/votacaobbbapi/controller/ParametroController.java
+++ backend/src/main/java/com/github/youssfbr/votacaobbbapi/controller/ParametroController.java
@@ -1,28 +1,30 @@
 package com.github.youssfbr.votacaobbbapi.controller;
 
 import com.github.youssfbr.votacaobbbapi.model.ParametroModel;
-import com.github.youssfbr.votacaobbbapi.repository.ParametroRepository;
+import com.github.youssfbr.votacaobbbapi.repository.IParametroRepository;
+
 import lombok.RequiredArgsConstructor;
+
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Optional;
+
 
 @RestController
 @RequestMapping("/api/parametros")
 @RequiredArgsConstructor
 public class ParametroController {
 
-    private final ParametroRepository repository;
+    private final IParametroRepository repository;
 
     @PostMapping("/salvar")
-    public ResponseEntity<ParametroModel> salvar(@RequestBody ParametroModel parametro) {
-        ParametroModel entidade = repository.save(parametro);
-        return ResponseEntity.ok(entidade);
+    public ParametroModel salvar(@RequestBody ParametroModel parametro) {
+        return repository.save(parametro);
     }
 
-    @GetMapping
-    public ResponseEntity<ParametroModel> consulta(@RequestParam String chave) {
+    @GetMapping("/consultar")
+    public ResponseEntity<ParametroModel> consultar(@RequestParam String chave) {
         Optional<ParametroModel> optParametro = repository.findById(chave);
         if (optParametro.isEmpty()) {
             return ResponseEntity.notFound().build();
--- backend/src/main/java/com/github/youssfbr/votacaobbbapi/model/ParametroModel.java
+++ backend/src/main/java/com/github/youssfbr/votacaobbbapi/model/ParametroModel.java
@@ -3,8 +3,10 @@ package com.github.youssfbr.votacaobbbapi.model;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
+
 
 @Data
 @AllArgsConstructor
